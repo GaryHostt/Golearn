103376d8d2653ee5e67bcb71429bee105eafdc4f
Work_laptop_Alex <alexmacdon94@gmail.com>
2019-07-26 14:13:17 -0700

    8

--- counter2.go
+++ counter2.go
@@ -17,6 +17,11 @@ func timeTrack(start time.Time, name string) {
 }
 
 func main() {
+        http.HandleFunc("/", handle)
+        http.HandleFunc("/_ah/health", healthCheckHandler)
+        log.Print("Listening on port 8080")
+        log.Fatal(http.ListenAndServe(":8080", nil))
+
         appengine.Main()
     start := time.Now()
 
@@ -41,6 +46,14 @@ func main() {
     go forever()
     select {} // block forever
 
+}
+
+func handle(w http.ResponseWriter, r *http.Request) {
+        if r.URL.Path != "/" {
+                http.NotFound(w, r)
+                return
+        }
+        fmt.Fprint(w, "Hello world!")
 }
 
 
